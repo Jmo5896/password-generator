afdb230c5a49e1f81976d287268a10c46f2537e4
Jmo5896 <jmo5896@moore-clan.net>
2019-10-09 15:41:03 -0600

    set up event listener form is working

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,9 @@
-console.log('working');
+document.getElementById('submitBtn').addEventListener('click', function(event) {
+    let charLen = document.getElementById('charLen').value;
+    let specialChar = document.getElementById('specialChar').checked;
+    let numChar = document.getElementById('numChar').checked;
+    let lowChar = document.getElementById('lowChar').checked;
+    let upChar = document.getElementById('upChar').checked;
+console.log(charLen, specialChar, numChar, lowChar, upChar);
+
+})
