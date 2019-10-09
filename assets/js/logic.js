517121c24645fa4b5f3ec7a15410518301c67696
Jmo5896 <jmo5896@moore-clan.net>
2019-10-09 15:45:08 -0600

    form clears out after closing

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,9 +1,15 @@
-document.getElementById('submitBtn').addEventListener('click', function(event) {
-    let charLen = document.getElementById('charLen').value;
-    let specialChar = document.getElementById('specialChar').checked;
-    let numChar = document.getElementById('numChar').checked;
-    let lowChar = document.getElementById('lowChar').checked;
-    let upChar = document.getElementById('upChar').checked;
-console.log(charLen, specialChar, numChar, lowChar, upChar);
+document.getElementById("submitBtn").addEventListener("click", function(event) {
+  let charLen = document.getElementById("charLen").value;
+  let specialChar = document.getElementById("specialChar").checked;
+  let numChar = document.getElementById("numChar").checked;
+  let lowChar = document.getElementById("lowChar").checked;
+  let upChar = document.getElementById("upChar").checked;
+  console.log(charLen, specialChar, numChar, lowChar, upChar);
+  document.getElementById("charLen").value = "";
+  document.getElementById("specialChar").checked = false;
+  document.getElementById("numChar").checked = false;
+  document.getElementById("lowChar").checked = false;
+  document.getElementById("upChar").checked = false;
 
-})
+  
+});
