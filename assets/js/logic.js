77b0536f3c23d36caaaa5d81b852347719acafea
Jmo5896 <jmo5896@moore-clan.net>
2019-10-09 15:55:51 -0600

    set up my password arrays for selecting the characters

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,3 +1,8 @@
+const alphas = "abcdefghijklmnopqrstuvwxyz".split("");
+const alphasUp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+const numbers = "1234567890".split("");
+const others = "`~!@#$%^&*()-_=+[{]};:,<.>/?".split("");
+
 document.getElementById("submitBtn").addEventListener("click", function(event) {
   let charLen = document.getElementById("charLen").value;
   let specialChar = document.getElementById("specialChar").checked;
@@ -10,6 +15,20 @@ document.getElementById("submitBtn").addEventListener("click", function(event) {
   document.getElementById("numChar").checked = false;
   document.getElementById("lowChar").checked = false;
   document.getElementById("upChar").checked = false;
-
-  
+  let passwordBank = [];
+  if (specialChar === true) {
+    passwordBank.push(others);
+  }
+  if (numChar === true) {
+    passwordBank.push(numbers);
+  }
+  if (lowChar === true) {
+    passwordBank.push(alphas);
+  }
+  if (upChar === true) {
+    passwordBank.push(alphasUp);
+  }
+  for (let i = 0; i < charLen; i++) {
+    
+  }
 });
